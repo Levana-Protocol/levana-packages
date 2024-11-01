645c0d1ebffd82dfd20c20abd4b24d7b11f04e9d
lvn-skate-dragon <skate-dragon@levana.exchange>
2024-11-01 07:42:28 +0200

    ensure types are accessible

--- packages/ui/src/ThemeProvider/index.tsx
+++ packages/ui/src/ThemeProvider/index.tsx
@@ -2,11 +2,49 @@ import { lazyLoad } from "@levana-protocol/utils/lazyLoad"
 import type { PropsWithFunctionChildren } from "@levana-protocol/utils/react"
 import CssBaseline from "@mui/joy/CssBaseline"
 import GlobalStyles from "@mui/joy/GlobalStyles"
+import type {} from "@mui/joy/ModalDialog"
 import { CssVarsProvider } from "@mui/joy/styles"
 import { type PropsWithChildren, useEffect, useState } from "react"
 
 import BreakpointDetection from "../BreakpointDetection"
 import type { CssVarsData } from "./types"
+
+declare module "@mui/joy/styles" {
+  interface ColorPalettePropOverrides {
+    info: false
+  }
+
+  interface FontSizeOverrides {
+    xs3: true
+    xs2: true
+    xl5: true
+    xl6: true
+    xl7: true
+  }
+
+  interface PaletteBackgroundOverrides {
+    // A better place to put these would be in a spotlight object, sibling to background. Until Joy provides that ability, this will do.
+    spotlight1: true
+    spotlight2: true
+    level4: true
+  }
+
+  interface Palette {
+    gradient: {
+      primary: string
+    }
+  }
+
+  interface TypographySystemOverrides {
+    caption: true
+  }
+}
+
+declare module "@mui/joy/ModalDialog" {
+  interface ModalDialogPropsLayoutOverrides {
+    auto: true
+  }
+}
 
 interface ThemeProviderProps {
   variant: "levana" | "rujira"
--- packages/ui/src/ThemeProvider/types.ts
+++ packages/ui/src/ThemeProvider/types.ts
@@ -1,43 +1,5 @@
-import type {} from "@mui/joy/ModalDialog"
 import type { PaletteRange, Theme } from "@mui/joy/styles/types"
 import type { Mode } from "@mui/system/cssVars/useCurrentColorScheme"
-
-declare module "@mui/joy/styles" {
-  interface ColorPalettePropOverrides {
-    info: false
-  }
-
-  interface FontSizeOverrides {
-    xs3: true
-    xs2: true
-    xl5: true
-    xl6: true
-    xl7: true
-  }
-
-  interface PaletteBackgroundOverrides {
-    // A better place to put these would be in a spotlight object, sibling to background. Until Joy provides that ability, this will do.
-    spotlight1: true
-    spotlight2: true
-    level4: true
-  }
-
-  interface Palette {
-    gradient: {
-      primary: string
-    }
-  }
-
-  interface TypographySystemOverrides {
-    caption: true
-  }
-}
-
-declare module "@mui/joy/ModalDialog" {
-  interface ModalDialogPropsLayoutOverrides {
-    auto: true
-  }
-}
 
 export type ColorRange = Pick<
   PaletteRange,
