9a75a87cec833b96a314c515a0b526fb61122a66
lvn-skate-dragon <skate-dragon@levana.exchange>
2024-11-03 09:20:24 +0200

    improve workflow

--- .ci/update-yarn-lock.sh
+++ .ci/update-yarn-lock.sh
@@ -7,9 +7,10 @@ yarn install --mode update-lockfile
 # Check for changes in yarn.lock
 if ! git diff --exit-code yarn.lock; then
   # Configure Git
-  git config --global user.name "github-actions[bot]"
-  git config --global user.email "github-actions[bot]@users.noreply.github.com"
-  
+  git config --global user.name "levana-bot"
+  git config --global user.email "bot@levana.finance"
+  git remote set-url origin https://$PAT@github.com/$REPOSITORY
+
   # Commit changes to yarn.lock
   git add yarn.lock
   git commit -m "update yarn.lock"
